b7f708fcdff7043a7423fd7269c167da5b5aaf27
krishnanshu_singh2301 <krishnanshusingh2005@gmail.com>
2026-04-25 17:25:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
+let ESP_IP = "";
+
 function connectESP() {
   ESP_IP = document.getElementById("espIp").value;
-  localStorage.setItem("esp_ip", ESP_IP);
+  alert("Connected to " + ESP_IP);
 }
 
-window.onload = function(){
-  let saved = localStorage.getItem("esp_ip");
-  if(saved){
-    ESP_IP = saved;
-    document.getElementById("espIp").value = saved;
-  }
+function espUrl(path){
+  return "http://" + ESP_IP + path;
 }
